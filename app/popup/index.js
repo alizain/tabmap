efeab1db1bd5678b311751695f9cfec7053d352a
Alizain Feerasta <alizain@Alizains-MacBook-Pro.local>
2016-01-17 15:05:36 -0500

    more mvp-complete state

--- app/popup/index.js
+++ app/popup/index.js
@@ -6,17 +6,21 @@ Promise.config({
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment-timezone';
 
 import tabs from '../core/tabs';
 import storage from '../core/storage';
 import { randomString } from '../core/utils';
 
-let Home = React.createClass({
+const TZ = moment.tz.guess();
+
+var Home = React.createClass({
 
   getInitialState() {
     return {
       input: '',
-      windows: {}
+      windows: {},
+      waiting: false
     };
   },
 
@@ -35,19 +39,14 @@ let Home = React.createClass({
     });
   },
 
-  captureWindow() {
-    tabs.capture().then((data) => {
-      storage.set(randomString(), {
-        tabs: data
-      });
-    });
-  },
-
   openWindow(key) {
+    this.setState({ waiting: true });
     let urls = this.state.windows[key].tabs.map((tab) => {
       return tab.url;
     });
-    tabs.open(urls);
+    tabs.open(urls).then(() => {
+      this.setState({ waiting: false });
+    });
   },
 
   removeWindow(key) {
@@ -56,13 +55,15 @@ let Home = React.createClass({
 
   handleSubmit(e) {
     e.preventDefault();
+    this.setState({ waiting: true });
     tabs.capture().then((data) => {
       storage.set(randomString(), {
         name: this.state.input,
         tabs: data,
-        saved: Date.now()
+        saved: new Date().toISOString()
       });
       this.state.input = '';
+      this.setState({ waiting: false });
     });
   },
 
@@ -72,17 +73,39 @@ let Home = React.createClass({
     });
   },
 
-  renderSpace(key, window) {
+  renderWindows() {
+    if (Object.keys(this.state.windows).length > 0) {
+      return (
+        <section>
+          <h2>Stored Windows</h2>
+          <ul className="windows-list">
+            {
+              Object.keys(this.state.windows).map((key) => {
+                return this.renderItem(key, this.state.windows[key]);
+              })
+            }
+          </ul>
+        </section>
+      );
+    }
+  },
+
+  renderItem(key, window) {
     return (
-      <li key={key}>
-        <div>{ !window.name ? key : window.name}</div>
-        <div className="pure-g">
-          <span className="pure-u-1-2">{window.saved}</span>
-          <button className="pure-u-3-8 pure-button button-open" onClick={this.openWindow.bind(this, key)}>
+      <li key={key} className="window window-row">
+        <div className="expand-flex details-row">
+          <div className="details-name">{ !!window.name ? window.name : key }</div>
+          <div className="window-row">
+            <div>{ window.tabs.length } tabs</div>
+            <div className="expand-flex details-date">{moment(window.saved).tz(TZ).fromNow()}</div>
+          </div>
+        </div>
+        <div className="button-row">
+          <button disabled={this.state.waiting} className="pure-button button button-open" onClick={this.openWindow.bind(this, key)}>
             <i className="fa fa-lg fa-external-link"></i>
             Open
           </button>
-          <button className="pure-u-1-8 pure-button button-remove" onClick={this.removeWindow.bind(this, key)}>
+          <button disabled={this.state.waiting} className="pure-button button button-remove" onClick={this.removeWindow.bind(this, key)}>
             <i className="fa fa-lg fa-trash"></i>
           </button>
         </div>
@@ -97,29 +120,18 @@ let Home = React.createClass({
           <h1>TabMap</h1>
         </section>
         <section>
-          <h3>Save all tabs in the current window</h3>
-          <form onSubmit={this.handleSubmit} className="pure-form">
-            <fieldset>
-              <input type="text"
-                value={this.state.input}
-                onChange={this.handleInput}
-                placeholder="Name"
-                className="pure-input-2-3"
-              />
-            <button type="submit" className="pure-button pure-button-primary">Save</button>
-            </fieldset>
+          <h2>Add Current window</h2>
+          <form onSubmit={this.handleSubmit} className="pure-form window-row">
+            <input type="text"
+              value={this.state.input}
+              onChange={this.handleInput}
+              placeholder="Give this window a name"
+              className="pure-input-2-3 form-item"
+            />
+            <button type="submit" className="pure-button pure-button-primary form-item">Save Window</button>
           </form>
         </section>
-        <section>
-          <h3>Saved windows</h3>
-          <ul className="window-list">
-            {
-              Object.keys(this.state.windows).map((key) => {
-                return this.renderSpace(key, this.state.windows[key]);
-              })
-            }
-          </ul>
-        </section>
+        { this.renderWindows() }
       </div>
     );
   }
